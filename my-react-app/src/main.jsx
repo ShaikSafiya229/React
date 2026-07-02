41329d990328a049a661c20670195495492e93db
ShaikSafiya229 <safiyashaik2006@gmail.com>
2026-07-02 09:21:34 +0530

    React_js

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Classic1 from './Events.jsx'
+import JSX from './crud.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Classic1></Classic1>
-
+    <JSX />
   </StrictMode>,
 )
